33d1aac2cffc3b29963b976edbba4cef004fba8e
Keely Lowder <keelybowman@icloud.com>
2024-12-15 23:04:11 -0500

    Fixed find vehicle by mileage method

--- src/main/java/com/pluralsight/dealership/CarDealershipAPI/VehicleController.java
+++ src/main/java/com/pluralsight/dealership/CarDealershipAPI/VehicleController.java
@@ -22,43 +22,43 @@ public class VehicleController {
         this.vehicleDAO = vehicleFromDataBase;
     }
 
-    // GET request to retrieve all vehicles.
+    // get request to retrieve all vehicles.
     @GetMapping("/allCars")
     public List<Vehicle> findAllVehicles() {
 
         return vehicleDAO.findAllVehicles();
     }
 
-    // GET request to find vehicles by price.
+    // get request to find vehicles by price.
     @GetMapping("/vehiclePrice")
     public List<Vehicle> findVehicleByPrice(@RequestParam("minPrice") double minPrice, @RequestParam("maxPrice") double maxPrice){
         return vehicleDAO.findVehicleByPrice(minPrice, maxPrice);
     }
 
-    // GET request to find vehicles by make and model.
+    // get request to find vehicles by make and model.
     @GetMapping("/vehicleMakeModel")
     public List<Vehicle> findVehicleByMakeModel(@RequestParam("make") String make, @RequestParam("model") String model){
         return vehicleDAO.findVehicleByMakeModel(make, model);
     }
 
-    // GET request to find vehicles by year
+    // get request to find vehicles by year
     @GetMapping("/vehicleYear")
     public List<Vehicle> findVehicleByYear(@RequestParam("minYear") int minYear, @RequestParam("maxYear") int maxYear) {
         return vehicleDAO.findVehicleByYear(minYear + maxYear);
     }
 
-    // GET request to find vehicles by color.
+    // get request to find vehicles by color.
     @GetMapping("/vehicleColor")
     public List<Vehicle> findVehicleByColor(@RequestParam("color") String color){
         return vehicleDAO.findVehicleByColor(color);
     }
-    // GET request to find vehicles by mileage
+    // get request to find vehicles by mileage
     @GetMapping("/vehicleMileage")
     public List<Vehicle> findVehiclesByMiles(@RequestParam("minMiles") int minMiles, @RequestParam("maxMiles") int maxMiles) {
         return vehicleDAO.findVehicleByMileage(minMiles, maxMiles);
     }
 
-    // GET request to find a vehicle by its VIN.
+    // get request to find a vehicle by its VIN.
     @GetMapping("/vehicleVin")
     public List<Vehicle> findVehicleByVin(@RequestParam("vin") int vin) {
         return vehicleDAO.findVehicleByVin(vin);
